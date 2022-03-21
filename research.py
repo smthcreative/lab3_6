51170cff8601b23b646666a766ae16d47d58a47d
smthcreative <93734464+smthcreative@users.noreply.github.com>
2022-03-22 01:41:40 +0200

    Create research.py

---
+++ research.py
@@ -0,0 +1,11 @@
+
+from notebook import Notebook, Note
+from menu import Menu
+
+var = 1
+notebook = Notebook()
+print(isinstance(var, object))
+print(isinstance(notebook.modify_memo, object))
+print(isinstance(Notebook, object))
+
+
